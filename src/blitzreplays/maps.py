9ee2f2150c0aa7dd489eddcafc24b5a6d0cb6e74
Jylpah <jylpah@gmail.com>
2023-07-08 17:23:43 +0300

    1st version: mypy OK

--- src/blitzreplays/maps.py
+++ src/blitzreplays/maps.py
@@ -2,13 +2,23 @@ from argparse import ArgumentParser, Namespace, SUPPRESS
 from configparser import ConfigParser
 from datetime import datetime
 from typing import Optional, Literal, Any, cast
+from pathlib import Path
+from os.path import isdir, isfile
+from re import Pattern, Match, compile
+import aiofiles
+import yaml
+from pydantic import ValidationError
 import logging
+
+from blitzutils import Map, Maps
 
 logger = logging.getLogger()
 error = logger.error
 message = logger.warning
 verbose = logger.info
 debug = logger.debug
+
+BLITZAPP_STRINGS: str = "Data/Strings/en.yaml"
 
 ########################################################
 #
@@ -39,7 +49,7 @@ def add_args(parser: ArgumentParser, config: Optional[ConfigParser] = None) -> b
         if not add_args_file(file_parser, config=config):
             raise Exception("Failed to define argument parser for: maps file")
 
-        if config is not None and "METADATA" in config:
+        if config is not None and "METADATA" in config.sections():
             configOptions = config["METADATA"]
             MAPS_FILE = configOptions.get("maps_json", MAPS_FILE)
 
@@ -64,7 +74,7 @@ def add_args_app(parser: ArgumentParser, config: Optional[ConfigParser] = None) 
     debug("starting")
     BLITZAPP_DIR: str = "./BlitzApp"
     try:
-        if config is not None and "METADATA" in config:
+        if config is not None and "METADATA" in config.sections():
             configOptions = config["METADATA"]
             BLITZAPP_DIR = configOptions.get("blitz_app_dir", BLITZAPP_DIR)
         parser.add_argument(
@@ -92,23 +102,56 @@ def add_args_file(parser: ArgumentParser, config: Optional[ConfigParser] = None)
 async def cmd(args: Namespace) -> bool:
     try:
         debug("starting")
+        maps_new: Maps | None
+        maps_old: Maps | None
+
         if args.maps_cmd == "app":
-            return await cmd_app(args)
-
+            if (maps_new := await cmd_app(args)) is None:
+                raise ValueError(f"could not read maps from game files: {args.blitz_app_dir}")
         elif args.maps_cmd == "file":
-            return await cmd_file(args)
-
+            if (maps_new := await cmd_file(args)) is None:
+                raise ValueError(f"could not read maps from file: {args.infile}")
         else:
-            raise NotImplementedError(f"unknown command: {args.tankopedia_cmd}")
+            raise NotImplementedError(f"unknown command: {args.maps_cmd}")
+
+        if args.update and isfile(args.outfile):
+            if (maps_old := await Maps.open_json(args.outfile)) is None:
+                error(f"could not parse old tankopedia ({args.outfile})")
+                return False
+            maps_old.update(maps_new)
+        else:
+            maps_old = maps_new
+
+        return await maps_old.save_json(args.outfile) > 0
 
     except Exception as err:
         error(f"{err}")
     return False
 
 
-async def cmd_app(args: Namespace) -> bool:
-    return False
+async def cmd_app(args: Namespace) -> Maps | None:
+    """read maps from game files"""
+    debug("starting")
+    maps = Maps()
+    filename: Path = Path(f"{args.blitz_app_dir}/{BLITZAPP_STRINGS}")
+    try:
+        debug(f"Opening file: %s for reading map strings", str(filename))
+        user_strs: dict[str, str] = dict()
+        with open(filename, "r", encoding="utf8") as strings_file:
+            user_strs = yaml.safe_load(strings_file)
+
+        re_map: Pattern = compile(r"^#maps:(\w+?):.+?$")
+
+        for key, value in user_strs.items():
+            # some Halloween map variants have the same short name
+            if re_map.match(key) and key not in maps:
+                maps.add(Map(name=value, key=key))
+        return maps
+    except Exception as err:
+        error(f"unable to read maps from {filename.resolve()}: {err}")
+    return None
 
 
-async def cmd_file(args: Namespace) -> bool:
-    return False
+async def cmd_file(args: Namespace) -> Maps | None:
+    debug("starting")
+    return await Maps.open_json(args.infile)
