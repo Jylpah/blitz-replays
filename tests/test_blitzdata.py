ae616c42869aba9e87b359409e74349d9eaca02d
Jylpah <jylpah@gmail.com>
2023-10-07 21:09:38 +0300

    eigenein's refactoring

--- tests/test_blitzdata.py
+++ tests/test_blitzdata.py
@@ -1,3 +1,4 @@
+import asyncio
 import sys
 import pytest  # type: ignore
 from typing import Sequence
@@ -5,17 +6,18 @@ import subprocess
 from os.path import dirname, realpath, join as pjoin, basename
 from pathlib import Path
 import aiofiles
+from click.testing import CliRunner, Result
 from pydantic import BaseModel
 from random import shuffle
 import logging
+
+from blitzreplays.blitzdata import cli
 
 logger = logging.getLogger()
 error = logger.error
 message = logger.warning
 verbose = logger.info
 debug = logger.debug
-
-sys.path.insert(0, str(Path(__file__).parent.parent.resolve() / "src"))
 
 from blitzutils import (
     Tank,
@@ -100,7 +102,6 @@ TANKOPEDIA_FILES = pytest.mark.datafiles(
 ########################################################
 
 
-@pytest.mark.asyncio
 @pytest.mark.parametrize(
     "args,added,updated",
     [
@@ -109,35 +110,25 @@ TANKOPEDIA_FILES = pytest.mark.datafiles(
     ],
 )
 @TANKOPEDIA_FILES
-async def test_1_blitz_data_tankopedia(
+def test_1_blitz_data_tankopedia(
     tmp_path: Path, datafiles: Path, args: list[str], added: int, updated: int
 ) -> None:
     OUTFILE: str = f'{tmp_path / "test_1_tankopedia.json"}'
     cmd: str = args[0]
     args[-1] = f"{(tmp_path / args[-1]).resolve()}"
 
-    completed_process = subprocess.run(
-        [
-            "python",
-            "-m",
-            "blitzreplays.blitzdata",
-            "tankopedia",
-            "--force",
-            "--outfile",
-            OUTFILE,
-        ]
-        + args
+    result: Result = CliRunner().invoke(
+        cli, ["tankopedia", "--force", "--outfile", OUTFILE, *args]
     )
-    assert completed_process.returncode == 0, f"blitzdata tankopedia {cmd} failed"
+    assert result.exit_code == 0, f"blitzdata tankopedia {cmd} failed"
 
     assert (
-        tankopedia := await WGApiWoTBlitzTankopedia.open_json(OUTFILE)
+        tankopedia := asyncio.run(WGApiWoTBlitzTankopedia.open_json(OUTFILE))
     ) is not None, f"could not open results: tankopedia {cmd}"
 
     assert len(tankopedia) == added, f"incorrect number of tanks: tankopedia {cmd}"
 
 
-@pytest.mark.asyncio
 @pytest.mark.parametrize(
     "args,added,updated",
     [
@@ -146,45 +137,31 @@ async def test_1_blitz_data_tankopedia(
     ],
 )
 @TANKOPEDIA_FILES
-async def test_2_blitz_data_tankopedia_update(
+def test_2_blitz_data_tankopedia_update(
     tmp_path: Path, datafiles: Path, args: list[str], added: int, updated: int
 ) -> None:
     OUTFILE: str = f"{tmp_path / TANKOPEDIA_OLD}"
     cmd: str = args[0]
     args[-1] = f"{(tmp_path / args[-1]).resolve()}"
 
-    completed_process = subprocess.run(
-        ["python", "-m", "blitzreplays.blitzdata", "tankopedia", "--outfile", OUTFILE]
-        + args
-    )
-    assert completed_process.returncode == 0, f"blitzdata tankopedia {cmd} failed"
+    result = CliRunner().invoke(cli, ["tankopedia", "--outfile", OUTFILE, *args])
+    assert result.exit_code == 0, f"blitzdata tankopedia {cmd} failed"
 
     assert (
-        tankopedia := await WGApiWoTBlitzTankopedia.open_json(OUTFILE)
+        tankopedia := asyncio.run(WGApiWoTBlitzTankopedia.open_json(OUTFILE))
     ) is not None, f"could not open results: tankopedia {cmd}"
 
     assert len(tankopedia) == added, f"incorrect number of tanks: tankopedia {cmd}"
 
 
-@pytest.mark.asyncio
-async def test_3_blitz_data_tankopedia_wg(tmp_path: Path) -> None:
+def test_3_blitz_data_tankopedia_wg(tmp_path: Path) -> None:
     OUTFILE: str = f"{tmp_path / 'tankopedia-exported.json'}"
 
-    completed_process = subprocess.run(
-        [
-            "python",
-            "-m",
-            "blitzreplays.blitzdata",
-            "tankopedia",
-            "--outfile",
-            OUTFILE,
-            "wg",
-        ]
-    )
-    assert completed_process.returncode == 0, f"blitzdata tankopedia wg failed"
+    result = CliRunner().invoke(cli, ["tankopedia", "--outfile", OUTFILE, "wg"])
+    assert result.exit_code == 0, f"blitzdata tankopedia wg failed"
 
     assert (
-        tankopedia := await WGApiWoTBlitzTankopedia.open_json(OUTFILE)
+        tankopedia := asyncio.run(WGApiWoTBlitzTankopedia.open_json(OUTFILE))
     ) is not None, f"could not open results: tankopedia wg"
 
     assert len(tankopedia) > 500, f"incorrect number of tanks: tankopedia wg"
