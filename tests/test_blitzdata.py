67e615421595e8439efece818606c400bd7db772
Jylpah <jylpah@gmail.com>
2023-12-02 12:10:08 +0200

    pytest for maps

--- tests/test_blitzdata.py
+++ tests/test_blitzdata.py
@@ -12,7 +12,6 @@ from pydantic import BaseModel
 from random import shuffle
 import logging
 
-
 # sys.path.insert(0, str(Path(__file__).parent.parent.resolve() / "src"))
 
 from blitzreplays.blitzdata import app
@@ -29,6 +28,10 @@ from blitzutils import (
     EnumVehicleTier,
     EnumVehicleTypeInt,
     EnumVehicleTypeStr,
+    Maps,
+    Map,
+    MapMode,
+    MapModeStr,
 )
 from blitzutils import WGApiWoTBlitzTankopedia
 
@@ -92,12 +95,21 @@ TANKOPEDIA_NEW: str = "01_Tankopedia_new.json"
 TANKOPEDIA_OLD: str = "01_Tankopedia_old.json"
 
 TANKOPEDIA_FILES = pytest.mark.datafiles(
-    FIXTURE_DIR / TANKOPEDIA_OLD,
-    FIXTURE_DIR / BLITZ_APP_DIR,
-    FIXTURE_DIR / TANKOPEDIA_NEW,
+    FIXTURE_DIR / TANKOPEDIA_OLD,  # 533 tanks
+    FIXTURE_DIR / BLITZ_APP_DIR,  # 609 tanks
+    FIXTURE_DIR / TANKOPEDIA_NEW,  # 609 tanks
     keep_top_dir=True,
 )
 
+MAPS_NEW: str = "02_maps_new.json"
+MAPS_OLD: str = "02_maps_old.json"
+
+MAPS_FILES = pytest.mark.datafiles(
+    FIXTURE_DIR / MAPS_NEW,  # 58 maps
+    FIXTURE_DIR / BLITZ_APP_DIR,  # 59 maps
+    FIXTURE_DIR / MAPS_OLD,  # 56 maps
+    keep_top_dir=True,
+)
 
 ########################################################
 #
@@ -134,19 +146,27 @@ def test_1_blitz_data_tankopedia(
 
 
 @pytest.mark.parametrize(
-    "args,added,updated",
+    "args,total,added",
     [
-        (["app", BLITZ_APP_DIR], 609, 0),
-        (["file", TANKOPEDIA_NEW], 609, 0),
+        (["app", BLITZ_APP_DIR], 609, 76),
+        (["file", TANKOPEDIA_NEW], 609, 76),
     ],
 )
 @TANKOPEDIA_FILES
 def test_2_blitz_data_tankopedia_update(
-    tmp_path: Path, datafiles: Path, args: list[str], added: int, updated: int
+    tmp_path: Path, datafiles: Path, args: list[str], total: int, added: int
 ) -> None:
     OUTFILE: str = f"{tmp_path / TANKOPEDIA_OLD}"
     cmd: str = args[0]
     args[-1] = f"{(tmp_path / args[-1]).resolve()}"
+
+    assert (
+        tankopedia := asyncio.run(WGApiWoTBlitzTankopedia.open_json(OUTFILE))
+    ) is not None, f"could not open results: tankopedia {cmd}"
+    tankopedia_tanks: int = len(tankopedia)
+    assert (
+        tankopedia_tanks == total - added
+    ), "incorrect number of tanks read from existing tankopedia"
 
     result = CliRunner().invoke(app, ["tankopedia", "--outfile", OUTFILE, *args])
     assert result.exit_code == 0, f"blitzdata tankopedia {cmd} failed"
@@ -155,7 +175,10 @@ def test_2_blitz_data_tankopedia_update(
         tankopedia := asyncio.run(WGApiWoTBlitzTankopedia.open_json(OUTFILE))
     ) is not None, f"could not open results: tankopedia {cmd}"
 
-    assert len(tankopedia) == added, f"incorrect number of tanks: tankopedia {cmd}"
+    assert len(tankopedia) == total, f"incorrect number of tanks: tankopedia {cmd}"
+    assert (
+        len(tankopedia) - tankopedia_tanks == added
+    ), f"incorrect number of tanks: tankopedia {cmd}"
 
 
 def test_3_blitz_data_tankopedia_wg(tmp_path: Path) -> None:
@@ -169,3 +192,70 @@ def test_3_blitz_data_tankopedia_wg(tmp_path: Path) -> None:
     ) is not None, f"could not open results: tankopedia wg"
 
     assert len(tankopedia) > 500, f"incorrect number of tanks: tankopedia wg"
+
+
+############################################
+#
+# Maps
+#
+############################################
+
+
+@pytest.mark.parametrize(
+    "args,added,updated",
+    [
+        (["app", BLITZ_APP_DIR], 59, 0),
+        (["file", MAPS_NEW], 58, 0),
+        (["file", MAPS_OLD], 56, 0),
+    ],
+)
+@MAPS_FILES
+def test_4_blitz_data_maps(
+    tmp_path: Path, datafiles: Path, args: list[str], added: int, updated: int
+) -> None:
+    OUTFILE: str = f'{tmp_path / "test_4_maps.json"}'
+    cmd: str = args[0]
+    args[-1] = f"{(tmp_path / args[-1]).resolve()}"
+
+    result: Result = CliRunner().invoke(
+        app, ["--force", "maps", "--outfile", OUTFILE, *args]
+    )
+    assert result.exit_code == 0, f"blitzdata maps {cmd} failed"
+
+    assert (
+        tankopedia := asyncio.run(Maps.open_json(OUTFILE))
+    ) is not None, f"could not open results: maps {cmd}"
+
+    assert len(tankopedia) == added, f"incorrect number of tanks: maps {cmd}"
+
+
+@pytest.mark.parametrize(
+    "args,total,added",
+    [
+        (["app", BLITZ_APP_DIR], 59, 3),
+        (["file", MAPS_NEW], 58, 2),
+    ],
+)
+@MAPS_FILES
+def test_5_blitz_data_maps_update(
+    tmp_path: Path, datafiles: Path, args: list[str], total: int, added: int
+) -> None:
+    OUTFILE: str = f"{tmp_path / MAPS_OLD}"
+    cmd: str = args[0]
+    args[-1] = f"{(tmp_path / args[-1]).resolve()}"
+
+    assert (
+        maps := asyncio.run(Maps.open_json(OUTFILE))
+    ) is not None, f"could not open results: maps {cmd}"
+    maps_N: int = len(maps)
+    assert maps_N == total - added, "incorrect number of tanks read from existing maps"
+
+    result = CliRunner().invoke(app, ["maps", "--outfile", OUTFILE, *args])
+    assert result.exit_code == 0, f"blitzdata maps {cmd} failed"
+
+    assert (
+        maps := asyncio.run(Maps.open_json(OUTFILE))
+    ) is not None, f"could not open results: maps {cmd}"
+
+    assert len(maps) == total, f"incorrect number of tanks: maps {cmd}"
+    assert len(maps) - maps_N == added, f"incorrect number of tanks: maps {cmd}"
