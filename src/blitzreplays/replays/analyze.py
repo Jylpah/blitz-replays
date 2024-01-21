0ce11a06c3210bfb36cd3683d107fd44b2f6cd72
Jylpah <jylpah@gmail.com>
2024-01-21 18:39:17 +0200

    move --player param to analyze command

--- src/blitzreplays/replays/analyze.py
+++ src/blitzreplays/replays/analyze.py
@@ -88,6 +88,13 @@ def analyze(
     reports_param: Annotated[
         str, Option("--reports", help="reports to create. use '+' to add extra reports")
     ] = "default",
+    player: Annotated[
+        Optional[int],
+        Option(
+            show_default=False,
+            help="player to analyze (WG account_id), default: player who recorded the replay",
+        ),
+    ] = None,
 ) -> None:
     """
     analyze replays
@@ -97,6 +104,10 @@ def analyze(
     reports = Reports()
     try:
         config: ConfigParser = ctx.obj["config"]
+        player = set_config(
+            config, fallback=0, section="WG", option="wg_id", value=player
+        )
+        ctx.obj["player"] = player
         if stats_type_param is None:
             stats_type_param = EnumStatsTypes[
                 set_config(
@@ -219,13 +230,6 @@ def analyze(
 @app.async_command()
 async def files(
     ctx: Context,
-    player: Annotated[
-        int,
-        Option(
-            show_default=False,
-            help="player to analyze (WG account_id), default: player who recorded the replay",
-        ),
-    ] = 0,
     wg_app_id: Annotated[Optional[str], Option(help="WG app ID")] = None,
     wg_region: Annotated[
         Optional[Region],
@@ -243,11 +247,14 @@ async def files(
     tankopedia: WGApiWoTBlitzTankopedia
     maps: Maps
     stats_type: StatsType
+    player: int
+
     try:
         config: ConfigParser = ctx.obj["config"]
         tankopedia = ctx.obj["tankopedia"]
         maps = ctx.obj["maps"]
         stats_type = ctx.obj["stats_type"]
+        player = ctx.obj["player"]
         wg_app_id = set_config(config, WG_APP_ID, "WG", "app_id", wg_app_id)
         region: Region
         if wg_region is None:
@@ -257,10 +264,9 @@ async def files(
         wg_rate_limit = set_config(
             config, WG_RATE_LIMIT, "WG", "rate_limit", wg_rate_limit
         )
-        player = set_config(config, 0, "WG", "wg_id", player)
 
     except KeyError as err:
-        error(f"could not read all the params: {err}")
+        error(f"could not read all the arguments: {err}")
         typer.Exit(code=2)
         assert False, "trick Mypy..."
 
@@ -290,6 +296,7 @@ async def files(
                         stats_cache=stats_cache,
                         tankopedia=tankopedia,
                         maps=maps,
+                        player=player,
                     )
                 )
             )
@@ -338,7 +345,11 @@ async def files(
         fields: FieldStore = ctx.obj["fields"]
         reports: Reports = ctx.obj["reports"]
         await analyze_replays(
-            replayQ=replayQ, stats_cache=stats_cache, fields=fields, reports=reports
+            replayQ=replayQ,
+            stats_cache=stats_cache,
+            fields=fields,
+            reports=reports,
+            player=player,
         )
         reports.print(fields=fields)
 
@@ -359,6 +370,7 @@ async def analyze_replays(
     stats_cache: StatsCache,
     fields: FieldStore,
     reports: Reports,
+    player: int = 0,
 ) -> EventCounter:
     """
     Apply stats to replays and perform analysis
@@ -394,6 +406,7 @@ async def replay_read_worker(
     stats_cache: StatsCache,
     tankopedia: WGApiWoTBlitzTankopedia,
     maps: Maps,
+    player: int = 0,
 ) -> EventCounter:
     """
     Async worker to read and pre-process replay files
@@ -424,7 +437,7 @@ async def replay_read_worker(
             continue
         try:
             if replay is not None:
-                await replay.enrich(tankopedia=tankopedia, maps=maps)
+                await replay.enrich(tankopedia=tankopedia, maps=maps, player=player)
                 await stats_cache.queue_stats(
                     replay, accountQ=accountQ, query_cache=query_cache
                 )
