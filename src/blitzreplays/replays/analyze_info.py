e912359823a690983ff382191d326af85274d910
Jylpah <jylpah@gmail.com>
2024-02-01 23:10:40 +0200

    renamed + more subcommands: metrics, filters

--- src/blitzreplays/replays/analyze_info.py
+++ src/blitzreplays/replays/analyze_info.py
@@ -11,7 +11,7 @@ import tomlkit
 from pyutils import AsyncTyper
 
 from .models_fields import Fields, ReportField
-from .args import read_param_list
+from .args import read_param_list, EnumTeamFilter, EnumGroupFilter
 
 
 logger = logging.getLogger()
@@ -28,16 +28,16 @@ FieldMetric = Enum("FieldMetric", dict(zip(metrics, metrics)))  # type: ignore
 
 
 @app.callback()
-def fields():
+def info():
     """
-    Report field config
+    Information of available for analysis
     """
 
 
-@app.command("list")
-def fields_list(ctx: Context):
+@app.command("fields")
+def info_list(ctx: Context):
     """
-    List configured fields
+    List configured report fields
     """
     field_store: Fields = ctx.obj["fields"]
     fields_param: str | None = ctx.obj["fields_param"]
@@ -46,7 +46,7 @@ def fields_list(ctx: Context):
     doc: TOMLDocument = tomlkit.document()
     doc.add("FIELDS", field_store.get_toml_field_sets())
     typer.echo()
-    typer.echo("Configured options for --fields FIELD_SET")
+    typer.echo("Configured options for --fields FIELD_SET:")
     typer.echo()
     typer.echo(tomlkit.dumps(doc))
     typer.echo()
@@ -57,19 +57,54 @@ def fields_list(ctx: Context):
     typer.echo(tomlkit.dumps(doc))
 
 
-@app.command("available")
-def fields_available():
+@app.command("metrics")
+def info_metrics():
     """
-    List available replay fields
+    List available field types / metrics
     """
     typer.echo()
-    typer.echo("Available main fields replays")
+    typer.echo("Available report field types:")
+    typer.echo()
+    for field_type in Fields.registry.values():
+        typer.echo(f"\tmetric={field_type.metric:<16}\t{field_type.__doc__.strip()}")
+        typer.echo()
+
+
+@app.command("filters")
+def info_filters():
+    """
+    List available player filters
+    """
+    typer.echo()
+    typer.echo("FIELD 'filter' filters replay players to include in FIELD calculations")
+    typer.echo()
+    typer.echo("Format: filter=team_filter:group_filter")
+    typer.echo()
+    typer.echo("Available 'team_filter' values:")
+    typer.echo()
+    for team_filter in EnumTeamFilter:
+        typer.echo(f"\t{team_filter.value}")
+    typer.echo()
+    typer.echo("Available 'group_filter' values:")
+    typer.echo()
+    for group_filter in EnumGroupFilter:
+        typer.echo(f"\t{group_filter.value}")
+    typer.echo()
+
+
+@app.command("replay")
+def info_replay():
+    """
+    List available fields in replays
+    """
+    typer.echo()
+    typer.echo("Available main fields replays:")
     typer.echo()
     for field in sorted(ReportField._replay_fields):
         typer.echo(f"\t{field}")
 
     typer.echo()
-    typer.echo("Available player fields in replays")
+    typer.echo("Available player fields in replays:")
     typer.echo()
 
     for field in sorted(ReportField._player_fields):
